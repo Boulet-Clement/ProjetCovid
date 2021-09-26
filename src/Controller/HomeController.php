bab7a71163541ee7caf1ea19d6eb5cb74b588800
Clément Boulet <clement.boulet60510@gmail.com>
2021-09-26 13:28:11 +0200

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,16 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
-namespace App\Controller;
 
 class HomeController
 {
-    private $view;
 
     public function __construct()
     {
         return;
     }
     
-    public function home()
+    public function home($request, $response, array $args)
     {
       // your code here
       // use $this->view to render the HTML
