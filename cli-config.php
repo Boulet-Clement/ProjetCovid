69e8646a9b82b2d21b27967d39ea8028218a7047
Boulet-Clement <clement.boulet60510@gmail.com>
2021-10-07 09:15:28 +0200

    il faut terminer le CLI

--- cli-config.php
+++ cli-config.php
@@ -1,41 +1,37 @@
 <?php
 
-use Slim\Factory\AppFactory;
 use DI\Container;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Doctrine\ORM\Tools\Setup;
+
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use DI\ContainerBuilder;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Symfony\Component\Console\Helper\HelperSet;
+
 
 require __DIR__ . '/vendor/autoload.php';
-
-require __DIR__ . '/config/bootstrap.php';
 
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();
 
+if (false) { // Should be set to true in production
+	$containerBuilder->enableCompilation(__DIR__ . '/var/cache');
+}
+
 // Set up settings
-$settings = require __DIR__ . './settings.php';
+$settings = require __DIR__ . '/config/settings.php';
 $settings($containerBuilder);
 
 // Set up dependencies
-$dependencies = require __DIR__ . './dependencies.php';
+$dependencies = require __DIR__ . '/config/dependencies.php';
 $dependencies($containerBuilder);
 
 
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
 
-// Instantiate the app
+
+return ConsoleRunner::createHelperSet($container->get(EntityManager::class));
+//return ConsoleRunner::createHelperSet($container[EntityManager::class]);
 
 
-return ConsoleRunner::createHelperSet($container[EntityManager::class]);
 
-
-
-//$app = \DI\Bridge\Slim\Bridge::create($container);
 
