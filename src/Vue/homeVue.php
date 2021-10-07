9a62e5202e282f23140a34ec93149fab8830bb25
BLOT <tristan.blot3@etu.univ-lorraine.fr>
2021-10-07 11:13:05 +0200

    navbar

--- src/Vue/homeVue.php
+++ src/Vue/homeVue.php
@@ -16,8 +16,24 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-  <!-- Navbar content -->
+	
+<nav class="navbar navbar-expand-lg navbar-light " style= "background-color: #ffe2af;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/auth">Se connecter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Aide</a>
+      </li>
+    </ul>
+  </div>
 </nav>
 </body>
 </html>
