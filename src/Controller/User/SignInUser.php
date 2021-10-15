4b4c8f689225d561c25d61fbd8914aff67b3a50e
Boulet-Clement <clement.boulet60510@gmail.com>
2021-10-15 09:04:10 +0200

    Un peu de revue de code

--- src/Controller/User/SignInUser.php
+++ src/Controller/User/SignInUser.php
@@ -4,36 +4,34 @@ use Doctrine\ORM\EntityManager;
 
 use App\Controller\BaseController;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Views\Twig;
 
 class SignInUser extends BaseController{
     private $em;
     private $userRepository;
-    private $twig;
     protected function action():Response
     {   
         $parsedBody = $this->request->getParsedBody();
         $username = htmlspecialchars($parsedBody['username']);
         $password =  htmlspecialchars($parsedBody['password']);
         if($this->is_login_ok($username,$password)){
-
+            $_SESSION['theme']="";
+            $_SESSION['message']="";
             $_SESSION['username'] = $username;
             return $this->response
             ->withHeader('location','/')
             ->withStatus(302);
         }else{
             $_SESSION['theme']="danger";
-            $_SESSION['message']="Mauvais identifiant";
+            $_SESSION['message']="Mauvais identifiants";
             return $this->response
             ->withHeader('location','/signin')
             ->withStatus(302);
         }
     }
-    public function __construct(EntityManager $em, Twig $twig)
+    public function __construct(EntityManager $em)
     {
         $this->em = $em;
         $this->userRepository = $em->getRepository('App\Model\User');
-        $this->twig = $twig;
     }
     private function is_login_ok($username,$password){
         $is_ok = false;
--- src/Controller/User/SignUpUser.php
+++ src/Controller/User/SignUpUser.php
@@ -6,8 +6,6 @@ use Doctrine\ORM\EntityManager;
 
 use App\Model\User;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Views\Twig;
-use Slim\Flash\Messages;
 
 require __DIR__ . '/../../Model/User.php';
 require __DIR__ . '/../../Repository/UserRepository.php';
@@ -15,8 +13,6 @@ require __DIR__ . '/../../Repository/UserRepository.php';
 class SignUpUser extends BaseController{
     private $em;
     private $userRepository;
-    private $twig;
-    private $message;
 
     protected function action():Response
     {   
@@ -26,22 +22,18 @@ class SignUpUser extends BaseController{
         $mail = htmlspecialchars($parsedBody['mail']);
         $login = htmlspecialchars($parsedBody['login']);
         $password =  htmlspecialchars($parsedBody['password']);
-        /*$test = $this->userRepository->findOneBy(array('username'=> 'test'));
-        var_dump($test);*/
         $user = new User(null,$login,$firstname,$lastname,$mail,$password);
         if(! $this->is_already_existing($user)){
             $this->em->persist($user);
-            /* Rediriger sur la page de connexion en disant bienvenue */
             $this->em->flush();
-            $this->message->addMessage('flashMessages','Le compte existe déjà');
-            $environment = $this->twig->getEnvironment();
-            $environment->addGlobal('flash', $this->message);
+            $_SESSION['theme']="success";
+            $_SESSION['message']='Votre compte à bien été enregistré, veuillez vous connecter pour vérifier.';
             return $this->response
             ->withHeader('location','/signin')
             ->withStatus(302);
         }else{
             $_SESSION['theme']="danger";
-            $_SESSION['message']='Le compte existe déjà';
+            $_SESSION['message']='Ce compte est déjà existant.';
             return $this->response
             ->withHeader('location','/signin')
             ->withStatus(302);
@@ -49,12 +41,10 @@ class SignUpUser extends BaseController{
         
         
     }
-    public function __construct(EntityManager $em, Twig $twig, Messages $message)
+    public function __construct(EntityManager $em)
     {
         $this->em = $em;
         $this->userRepository = $em->getRepository('App\Model\User');
-        $this->twig = $twig;
-        $this->message = $message;
     }
     private function is_already_existing($user)
     {
