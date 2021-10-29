538baffa37499430bfab9cdc1215b614bab55bd0
Boulet-Clement <clement.boulet60510@gmail.com>
2021-10-29 08:59:17 +0200

    Ajout du lien d'ajout d'un membre lorsque l'utilisateur est admin du groupe

--- public/javascript/group/group.js
+++ public/javascript/group/group.js
@@ -2,25 +2,41 @@ $('#link_description').on('click', function (e) {
     $('#content_description').show();
     $('#content_chat').hide();
     $('#content_membres').hide();
+    $('#content_ajout').hide();
     $('#link_description').addClass("active")
     $('#link_chat').removeClass("active")
     $('#link_membres').removeClass("active")
+    $('#link_ajout').removeClass("active")    
 })
 
 $('#link_chat').on('click', function (e) {
     $('#content_chat').show();
     $('#content_description').hide();    
     $('#content_membres').hide();
+    $('#content_ajout').hide();
     $('#link_chat').addClass("active")
     $('#link_membres').removeClass("active")
     $('#link_description').removeClass("active")
+    $('#link_ajout').removeClass("active")
 })
 
 $('#link_membres').on('click', function (e) {
     $('#content_membres').show();
     $('#content_chat').hide();
     $('#content_description').hide();
+    $('#content_ajout').hide();
     $('#link_membres').addClass("active")
+    $('#link_description').removeClass("active")
+    $('#link_chat').removeClass("active")
+    $('#link_ajout').removeClass("active")
+})
+$('#link_ajout').on('click', function (e) {
+    $('#content_ajout').show();
+    $('#content_membres').hide();
+    $('#content_chat').hide();
+    $('#content_description').hide();
+    $('#link_ajout').addClass("active")
+    $('#link_membres').removeClass("active")
     $('#link_description').removeClass("active")
     $('#link_chat').removeClass("active")
 })
