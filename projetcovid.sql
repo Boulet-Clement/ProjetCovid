34b0342d38b04db82768f91aa68c3330b45b41b8
Clément Boulet <clement.boulet60510@gmail.com>
2021-11-01 19:21:17 +0100

    Export de la base de données

--- projetcovid.sql
+++ projetcovid.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  mer. 13 oct. 2021 à 07:42
+-- Généré le : lun. 01 nov. 2021 à 18:20
 -- Version du serveur :  5.7.24
--- Version de PHP :  7.2.19
+-- Version de PHP : 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,45 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `projetcovid`
+-- Base de données : `projetcovid`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages_des_groupes`
+--
+
+CREATE TABLE `messages_des_groupes` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `sendDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages_groups`
+--
+
+CREATE TABLE `messages_groups` (
+  `groupmessage_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -34,20 +70,58 @@ CREATE TABLE `users` (
   `firstName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `lastName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `contaminated` tinyint(1) NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `latitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `longitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `joinDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `mail`, `password`) VALUES
-(1, 'test', 'pierre', 'Jean', 'test@gmail.com', 'salut'),
-(3, 'unTest', 'Paul', 'Jacques', 'teazeazast@gmail.com', 'oui');
+INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `mail`, `contaminated`, `password`, `latitude`, `longitude`, `joinDate`) VALUES
+(1, 'test', 'Clement', 'Boulet', 'contact@clement-boulet.fr', 0, '$2y$10$O6VXqBpkwoivyRVnBvbNjOkshyCsaU311VA2xGHB/s/FGpltx42FG', '49.7448', '2.1525', '2021-11-01'),
+(2, 'tristan', 'Tristan', 'Blot', 'tristant@blot.fr', 1, '$2y$10$azWswH02lCNMZm6G4keEN.k1GNOgt17Y/BkIj1STFjqIGp8DxCRNO', '49.7458', '2.1515', '2021-11-01'),
+(3, 'polomeiller', 'Paul', 'Meilleraye', 'paul@gmail.com', 0, '$2y$10$rnMVV0Fyf78DcHDGYJBFTOknudujNXWmYXw2qNjwUx3FZkvYJKC8O', '49.7433', '2.1545', '2021-11-01');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_F06D39705E237E06` (`name`),
+  ADD KEY `IDX_F06D3970642B8210` (`admin_id`);
+
+--
+-- Index pour la table `messages_des_groupes`
+--
+ALTER TABLE `messages_des_groupes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `messages_groups`
+--
+ALTER TABLE `messages_groups`
+  ADD PRIMARY KEY (`groupmessage_id`,`group_id`),
+  ADD KEY `IDX_370F93E6DE55EED5` (`groupmessage_id`),
+  ADD KEY `IDX_370F93E6FE54D947` (`group_id`);
 
 --
 -- Index pour la table `users`
@@ -58,14 +132,58 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `UNIQ_1483A5E95126AC48` (`mail`);
 
 --
+-- Index pour la table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`user_id`,`group_id`),
+  ADD KEY `IDX_FF8AB7E0A76ED395` (`user_id`),
+  ADD KEY `IDX_FF8AB7E0FE54D947` (`group_id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `messages_des_groupes`
+--
+ALTER TABLE `messages_des_groupes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `FK_F06D3970642B8210` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `messages_groups`
+--
+ALTER TABLE `messages_groups`
+  ADD CONSTRAINT `FK_370F93E6DE55EED5` FOREIGN KEY (`groupmessage_id`) REFERENCES `messages_des_groupes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_370F93E6FE54D947` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `FK_FF8AB7E0A76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_FF8AB7E0FE54D947` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
